0486b8f7525f321bf374c6dc909d1fd1e10e7cc8
190101101 <190101101@mail.ru>
2023-11-10 21:56:13 +0400

    1.3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const App = () => {
   return (
     <>
       <h1>hello cookie</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Suscipit incidunt eaque quaerat aliquid dolores, quae, mollitia omnis! Qui ratione iste, omnis error. Molestias, quibusdam et optio dolores eligendi accusantium veniam?</p></p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Suscipit incidunt eaque quaerat aliquid dolores, quae, mollitia omnis! Qui ratione iste, omnis error. Molestias, quibusdam et optio dolores eligendi accusantium veniam?</p>
     </>
   );
 }
